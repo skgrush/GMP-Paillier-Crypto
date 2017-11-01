82571f4b64d96aff2be6101db0f75949304b4188
Samuel K. Grush <Samuel@Grush.org>
2017-10-31 21:17:49 -0500

    figured out arbitrary line input.

--- src/main.c
+++ src/main.c
@@ -7,33 +7,38 @@
 
 int main(int argc, char *argv[]) {
 
-  char *pqg_filename;
+  char *pqg_filename = NULL;
   FILE *pqg_file;
-  char *lambdamu_filename;
+  char *lambdamu_filename = NULL;
   FILE *lambdamu_file;
-  char *vectorU_filename;
+  char *vectorU_filename = NULL;
   FILE *vectorU_file;
-  char *encU_filename;
+  char *encU_filename = NULL;
   FILE *encU_file;
-  char *vectorV_filename;
+  char *vectorV_filename = NULL;
   FILE *vectorV_file;
-  char *encV_filename;
+  char *encV_filename = NULL;
   FILE *encV_file;
-  char *output_filename;
+  char *output_filename = NULL;
   FILE *output_file;
 
+  size_t len = 0;
   printf("Enter the name of the file that contains p, q, and g: ");
-  scanf("%s", &pqg_filename);
+  getline(&pqg_filename, &len, stdin);
   printf("Enter the output file name to store LAMBDA and MU: ");
-  scanf("%s", &lambdamu_filename);
-  printf("Enter the name of the file that contains vector-u: ");
-  scanf("%s", &vectorU_filename);
+  getline(&lambdamu_filename, &len, stdin);
+  printf("\nEnter the name of the file that contains vector-u: ");
+  getline(&vectorU_filename, &len, stdin);
   printf("Enter the output file name to store E(u): ");
-  scanf("%s", &encU_filename);
+  getline(&encU_filename, &len, stdin);
   printf("Enter the name of the file that contains vector-v: ");
-  scanf("%s", &vectorV_filename);
+  getline(&vectorV_filename, &len, stdin);
   printf("Enter the output file name to store E(v): ");
-  scanf("%s", &encV_filename);
+  getline(&encV_filename, &len, stdin);
   printf("Enter the output file name to store E(u.v) and u.v: ");
-  scanf("%s", &output_filename);
+  getline(&output_filename, &len, stdin);
+
+  printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n", pqg_filename, lambdamu_filename,
+         vectorU_filename, encU_filename, vectorV_filename, encV_filename,
+         output_filename);
 }
