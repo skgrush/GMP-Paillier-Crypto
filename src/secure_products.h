d0d01348691d454f156ed8b7e95b9a36a2cfdffe
Shwam <wsmkd6@mst.edu>
2017-11-03 22:03:38 -0500

    Document homomorphic stuff

--- src/secure_products.h
+++ src/secure_products.h
@@ -39,14 +39,45 @@ void secureMult_P2_makeHp(mpz_t Hp,
 /**
  * Multiplies using ciphertext-by-plaintext approach
  *
+ * encProduct <- E(A)^B mod N^2
+ *
+ * Arguments:
+ *  encProduct  output encrypted product of A*B (E(A*B))
+ *  A           input E(A)
+ *  B           input B
+ *  pub         public key
  */
 void homomorphicMult(mpz_t encProduct, const mpz_t A, const mpz_t B, 
                   const PaillierPublicKey pub);
 
+
+/**
+ * Adds two paillier encrypted values
+ *
+ * result <- E(A+B)
+ *
+ * Arguments:
+ *  result      output encrypted sum of A+B (E(A+B))
+ *  A           input E(A)
+ *  B           input E(B)
+ *  pub         public key
+ */
 void homomorphicAdd(mpz_t result, 
                     const mpz_t A, const mpz_t B, 
                     const PaillierPublicKey pub);
 
+/**
+ * Encrypted dot product of one encrypted and one plaintext vector
+ *
+ * UdotV <- E(u.v)
+ *
+ * Arguments:
+ *  UdotV       output dot product of u.v
+ *  U           input encrypted vector U
+ *  V           input plaintext vector V
+ *  LEN         input vector length
+ *  pub         public key
+ */
 void dotProd(mpz_t UdotV, const mpz_t *U, const mpz_t *V, const int LEN, 
              const PaillierPublicKey pub);
 
